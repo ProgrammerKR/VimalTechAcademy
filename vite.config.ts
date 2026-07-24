0743382416562a6faddd63280dc36e774a93585e
Prog. Kanishk Raj <programmerkr.123@gmail.com>
2026-07-24 13:26:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/YOUR_REPOSITORY_NAME/",
+  base: "/VimalTechAcademy/",
 });
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
